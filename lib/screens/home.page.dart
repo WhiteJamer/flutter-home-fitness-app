db023656a59b778e7b0186eb598e1a7e1e141c73
whitejamer <mihailkuzin.job@yandex.ru>
2022-09-15 17:51:07 +0300

    Add more animations in Homepage

--- lib/screens/home.page.dart
+++ lib/screens/home.page.dart
@@ -1,10 +1,43 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:myapp/enums/colors.enums.dart';
 import 'package:myapp/screens/stepshow.page.dart';
+import 'package:myapp/utils/svg.utils.dart';
+import 'package:myapp/widgets/button.widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late Animation<double> _leftDBellPositionAnimation;
+  late Animation<double> _rightDBellPositionAnimation;
+  late AnimationController _positionAnimController;
+
+  @override
+  void initState() {
+    super.initState();
+    _positionAnimController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 1750));
+    _leftDBellPositionAnimation = Tween(begin: 5.5, end: 4.0).animate(
+        CurvedAnimation(
+            parent: _positionAnimController, curve: Curves.easeInOut));
+    _rightDBellPositionAnimation = Tween(begin: 4.0, end: 5.5).animate(
+        CurvedAnimation(
+            parent: _positionAnimController, curve: Curves.easeInOut));
+    _positionAnimController.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _positionAnimController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,83 +52,64 @@ class HomePage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: BackgroundColor,
-      body: Stack(
-        children: [
-          Positioned(
-            top: 55, // TODO Do Adaptive position for devices
-            right: 14,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                IconButton(
-                  splashRadius: 24,
-                  onPressed: () {},
-                  iconSize: 24,
-                  icon: SvgPicture.asset(
-                    "assets/icons/notification.svg",
-                  ),
-                ),
-                IconButton(
-                  splashRadius: 24,
-                  onPressed: () {},
-                  iconSize: 24,
-                  icon: SvgPicture.asset(
-                    "assets/icons/settings.svg",
-                  ),
-                ),
-              ],
+      body: AnimatedBuilder(
+        animation: _positionAnimController,
+        builder: (context, child) => Stack(
+          children: [
+            Positioned(
+              top: MediaQuery.of(context).size.height / 5.5,
+              left: MediaQuery.of(context).size.width / 5,
+              child: renderLogo(),
             ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            padding: EdgeInsets.only(top: 14, left: 14, right: 14),
-            child: Column(
+            Positioned(
+                top: MediaQuery.of(context).size.height /
+                    _leftDBellPositionAnimation.value,
+                left: MediaQuery.of(context).size.width / 5 - 15,
+                child: Transform.rotate(
+                  angle: pi / 5,
+                  child: SvgPicture.asset("assets/dumbbell.svg"),
+                )),
+            Positioned(
+                top: MediaQuery.of(context).size.height /
+                    _rightDBellPositionAnimation.value,
+                right: MediaQuery.of(context).size.width / 5 - 15,
+                child: Transform.rotate(
+                  angle: -pi / 5,
+                  child: SvgPicture.asset("assets/dumbbell.svg"),
+                )),
+            Positioned(
+              top: 55, // TODO Do Adaptive position for devices
+              right: 14,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Button(
+                    icon: renderNotificationIcon(),
+                  ),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Button(
+                    icon: renderSettingsIcon(),
+                  ),
+                ],
+              ),
+            ),
+            Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                // SvgLogo
-                SvgPicture.asset(
-                  "assets/logo.svg",
-                  height: 150,
-                ),
                 SizedBox(
                   height: 30,
                 ),
-                ElevatedButton(
-                  onPressed: _startAFitness,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("START A FITNESS"),
-                      SvgPicture.asset(
-                        "assets/icons/arrow-right.svg",
-                        color: PrimaryColor,
-                        height: 22,
-                      ),
-                      // Arrow Right Icon SVG
-                    ],
-                  ),
-                ),
+                Button(text: "START A FITNESS", cb: _startAFitness),
                 SizedBox(
                   height: 30,
                 ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("CHANGE A PROGRAM",),
-                      SvgPicture.asset(
-                        "assets/icons/pencil.svg",
-                        color: PrimaryColor,
-                        height: 22,
-                      ),
-                    ],
-                  ),
-                ),
+                Button(text: "CHANGE PROGRAM", cb: _startAFitness),
               ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
